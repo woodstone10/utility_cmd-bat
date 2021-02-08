1cf6c0fbb779fe382da52448e8a9d01708acb26c
Jonggil Nam <woodstone10@gmail.com>
2021-02-08 20:54:59 +0900

    qualcomm_logs_to_7z.bat
    Compress Qualcomm logs (isf, hdf, qmdl, def) with 7zip

---
+++ qualcomm_logs_to_7z.bat
@@ -0,0 +1,4 @@
+for /f %%a IN ('dir /b *.isf') do C:\"Program Files"\7-Zip\7z.exe a %%a".7z" %%a
+for /f %%a IN ('dir /b *.hdf') do C:\"Program Files"\7-Zip\7z.exe a %%a".7z" %%a
+for /f %%a IN ('dir /b *.dlf') do C:\"Program Files"\7-Zip\7z.exe a %%a".7z" %%a
+for /f %%a IN ('dir /b *.qmdl') do C:\"Program Files"\7-Zip\7z.exe a %%a".7z" %%a
